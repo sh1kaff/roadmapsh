eb4ab083ca08afed51c5ed378b7bdce18029f3df
Big Brother <aykibugz@yandex.ru>
2025-02-09 00:02:43 +0400

    json lib

--- task-cli/main.cpp
+++ task-cli/main.cpp
@@ -1,32 +1,13 @@
 #include <iostream>
 #include "task.h"
-#include "json.hpp"
+#include "json/single_include/nlohmann/json.hpp"
 
+using json = nlohmann::json;
  
 int main() {
-    int a;
+    std::string Data = R"({ "name": "Alex", "role": "admin" })";
 
-    TasksList list;
+    json Doc = json::parse(Data);
 
-    list.addTask(std::string("First"));
-    list.addTask(std::string("Second"));
-    list.addTask(std::string("Third"));
-    list.addTask(std::string("Fourth"));
-
-    std::cin >> a;
-
-    list.markStatus(3, Status::DONE);
-    list.editTask(1, std::string("Second Edit"));
-
-    std::cout << list << "\n";
-
-    std::cin >> a;
-
-    list.delTask(2);
-
-    std::cout << list << "\n";
-
-    list.addTask(std::string("some"));
-
-    list.printByStatus(Status::ANY);
+    std::cout << Doc["name"];
 }
